988bf627cbfd1e842e46911d904ae0ac207e1eee
Rajan Walia <rajanwalia4@gmail.com>
2024-04-24 00:04:31 +0530

    setting up rooms

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Multiplayer Tic Tac Toe
+
+## Functional Requirements
+- User should be able to create the game room.
+- User should be able to join the game room.
+- Only 2 users can join a certain room.
+- There should be a timer of X=30 seconds to finish game.
+
+
+## Tech Stack
+- NodeJS, socket.io
--- server.js
+++ server.js
@@ -1,38 +1,107 @@
-const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
-const { Server } = require('socket.io');
-const cors = require("cors");
+import   express  from  'express';
+import { createServer }  from 'node:http';
+import { join } from 'node:path';
+import  { Server } from 'socket.io';
+import cors  from "cors";
+import  {nanoid} from "nanoid";
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
 app.use(cors());
 
+const rooms = [];
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
+// app.post("/join-room",(req,res)=>{
+function joinRoom(roomId){
+  for(let room of rooms){
+    if(room.roomId == roomId){
+      // need to send the socket to the user who have created the room.
+      return {
+        roomId,
+        success:true,
+        message : "Joined the room successfully"
+      }
+
+    }
+  }
+  console.log("Rooms not found : ",rooms);
+  return {
+    roomId,
+    success:false,
+    message : "Invalid roomId"
+  }
+}
+// });
+
+// app.get("/create-room",(req,res)=>{
+function createRoom(id){
+  const roomId = id || nanoid(10);
+  const time = Date.now();
+  rooms.push({
+    roomId,
+    time
+  });
+
+  return {roomId, time, message : "Waiting for the player to join the room"};
+}
+// });
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    // console.log(socket)
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
+  console.log('a user connected', socket.id);
+  // console.log(socket)
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 
-    socket.on('mark-point', (data) => {
-        console.log(data);
-        // socket.broadcast("set-points",{data});
-        io.emit("set-points",{data});
-    });
+  socket.on('mark-point', (data) => {
+      console.log(data);
+      // socket.broadcast("set-points",{data});
+      io.emit("set-points",{data});
+  });
 
-    // socket.on('set-points', (data) => {
-    //     console.log(data);
-    //     socket.emit("broadcast",{data});
-    // });
+
+  // socket.on("create-room",(data)=>{
+  //   console.log("rooms", socket.rooms);
+  //   // created the room
+  //   const roomInfo = createRoom();
+  //   console.log("roomInfo ",roomInfo);
+  //   socket.join(roomInfo.roomId);
+  //   io.to(roomInfo.roomId).emit("create-room", roomInfo);
+  //   // io.emit("create-room", roomInfo);
+  // })
+
+
+  socket.on("join-room",(data)=>{
+    console.log("before rooms arrays : ", rooms);
+    console.log("Socket rooms : ", socket.rooms);
+    // created the room
+    console.log("roomId: ", data);
+    const roomInfo = joinRoom(data.roomId);
+    // If room already exist and there are already 2 people then don't allow others to join the same room
+    if(roomInfo.success){
+      console.log("room Found : ",roomInfo.roomId);
+      socket.join(roomInfo.roomId);
+      io.to(roomInfo.roomId).emit("join-room", {roomInfo,socketId : socket.id});
+    }else{
+      const roomInfo = createRoom(data.roomId);
+      console.log("roomInfo ",roomInfo);
+      socket.join(roomInfo.roomId);
+      io.to(roomInfo.roomId).emit("join-room", {roomInfo, socketId : socket.id});
+    }
+    console.log("after rooms arrays : ", rooms);
+  })
+
+  // socket.on('set-points', (data) => {
+  //     console.log(data);
+  //     socket.emit("broadcast",{data});
+  // });
 });
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
-});
+})
